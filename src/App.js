7cc9275108ca03126d09a0c8d1b80652fa227c08
RahulFernando <rahulrulz680@gmail.com>
2022-04-17 12:17:24 +0530

    feat: send cart data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,79 @@
-import { useSelector } from 'react-redux';
+import { useEffect, Fragment } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+
+import { setNotification } from './redux/ui-slice';
+
+let isInitial = true;
 
 function App() {
-  const cartVisible = useSelector(state => state.ui.cartVisible);
+  const dispatch = useDispatch();
+
+  const cartVisible = useSelector((state) => state.ui.cartVisible);
+  const notification = useSelector((state) => state.ui.notification);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        setNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data',
+        })
+      );
+
+      const response = await fetch(
+        'https://react-http-3606c-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
+        { method: 'PUT', body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed!');
+      }
+
+      dispatch(
+        setNotification({
+          status: 'success',
+          title: 'Success',
+          message: 'Sent cart data success',
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        setNotification({
+          status: 'error',
+          title: 'Failed',
+          message: 'Sending cart data failed!',
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {cartVisible && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {cartVisible && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/redux/ui-slice.js
+++ src/redux/ui-slice.js
@@ -1,20 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const uiSlice = createSlice({
-    name: 'ui',
-    initialState: { cartVisible: true },
-    reducers: {
-        toggle(state) {
-            return {
-                ...state,
-                cartVisible: !state.cartVisible
-            }
-        }
-    }
-})
+  name: 'ui',
+  initialState: { cartVisible: true, notification: null },
+  reducers: {
+    toggle(state) {
+      return {
+        ...state,
+        cartVisible: !state.cartVisible,
+      };
+    },
+    setNotification(state, action) {
+      return {
+        ...state,
+        notification: action.payload,
+      };
+    },
+  },
+});
 
 const { actions, reducer } = uiSlice;
 
-export const { toggle } = actions;
+export const { toggle, setNotification } = actions;
 
 export default reducer;
